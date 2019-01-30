82cb2502af9a37debbb68cce3ed807dc38531718
Pavel Kulhavy <pavel@pavelkulhavy.cz>
2019-01-30 10:29:49 +0100

    Přidat druhý button

--- GithubTest/Form1.cs
+++ GithubTest/Form1.cs
@@ -21,5 +21,10 @@ namespace GithubTest
         {
             MessageBox.Show("Tuk tuk");
         }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tuk tuk tuk");
+        }
     }
 }
