14e1ecf3cb208ace25651821f853ee0a7c55c6fc
Pavel Kulhavy <pavel@pavelkulhavy.cz>
2019-01-30 09:44:17 +0100

    Tuk tuk

--- GithubTest/Form1.cs
+++ GithubTest/Form1.cs
@@ -16,5 +16,10 @@ namespace GithubTest
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Tuk tuk");
+        }
     }
 }
